de5ecca33b9207e0de8eb0c5bba3f3f360de76e0
Crowdbotics <team@crowdbotics.com>
2022-12-30 17:21:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "trace_payment_38131",
+  url: "https://trace_payment_38131.botics.co",
+  api: "https://trace_payment_38131.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
